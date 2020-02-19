4b0f85b2a5ff44375ff32d34949e95929fa012fe
unn04012 <unn04012@naver.com>
2020-02-20 02:28:31 +0900

    글쓰기 업로드 처리

---
+++ writeboard_update.php
@@ -0,0 +1,32 @@
+<?php
+include("../login/connect.php");
+
+$mb_id = $_SESSION['ss_mb_id'];
+
+if(!$mb_id){
+  echo "<script> alert('로그인 후 이용 가능합니다'); </script>";
+  echo "<script> location.replace('../login/login.php'); </script>";
+}
+
+$mb_post_datetime = date('Y-m-d', time());
+$mb_title   = trim($_POST['title']);
+$mb_content = trim($_POST['content']);
+
+$sql = "INSERT INTO notice
+                SET mb_id = '$mb_id',
+                    mb_post_datetime = '$mb_post_datetime',
+                    mb_title = '$mb_title',
+                    mb_content = '$mb_content'";
+$result = mysqli_query($conn, $sql);
+
+
+if($result){
+  echo "<script> alert('성공적으로 업로드 되었습니다'); </script>";
+  echo "<script> location.replace('./noticeboard.php');</script>";
+  exit;
+}else{
+  echo "<script> alert('업로드실패'); </script>";
+  echo "<script> location.replace('./writeboard.php');</script>";
+  exit;
+}
+ ?>
